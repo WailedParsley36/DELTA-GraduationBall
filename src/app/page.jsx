c8b7ca40d1bbfe80a94c4284fc05f062ebd8861f
Wailed Parsley 36 <71410855+WailedParsley36@users.noreply.github.com>
2024-09-09 08:52:07 +0200

    Grammar Error

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -464,7 +464,7 @@ export default async function Home() {
           </div>
           <div className="max-w-2xl">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              Chcete se zůčastnit?
+              Chcete se zúčastnit?
             </h1>
             <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
               Neváhejte a zarezervujte si své místo na DELTA Maturitním plese
