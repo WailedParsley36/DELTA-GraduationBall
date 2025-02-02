d4baf09f4ca903e9753e16054b014dbc258d71ac
Wailed Parsley 36 <71410855+WailedParsley36@users.noreply.github.com>
2025-02-02 11:43:04 +0100

    Ubytovani redirect

---
+++ src/app/ubytovani/page.jsx
@@ -0,0 +1,6 @@
+import {redirect} from 'next/navigation'
+
+export default function Ubytovani() {
+    redirect("https://docs.google.com/forms/d/e/1FAIpQLScjrqnRk8ktcLTL6tf4MH4nEb4N52DjcDMRvyhIM8SbSkuZyg/viewform?usp=dialog")
+    return <></>
+}
